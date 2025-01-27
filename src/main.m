92f9fb999f524e88544ebf03c6610727c2c8ee6c
Gabriel Ratschiller <gabriel.ratschiller1998@gmail.com>
2025-01-27 16:57:27 +0100

    add greyscale conversion, add median filter and adaptive histogram equalization

--- src/main.m
+++ src/main.m
@@ -1,8 +1,35 @@
 % main pipeline with all the other functions
 function [] = main(input_image)
     
-    % 1. Convert RGB-image to grayscale image
-    image_grey(:, :, 1) = 0.114 .* input_image(:, :, 1) + 0.587 .* input_image(:, :, 2) + 0.299 .* input_image(:, :, 3);
-    imshow(image_grey, 'Parent', axes);
+    app = app_GUI();
 
+    %% 1. convert RGB-image to grayscale image
+    % check if the input is a valid RGB image (3 channels)
+    if size(input_image, 3) ~= 3
+        error('Input image must be RGB (3 channels).');
+    end
+
+    gray_image = rgb2gray(input_image);
+    plotImage(gray_image, app.axes_2);
+
+    %% 2. median filter to remove noise
+    % set filter parameters
+    sigma = [3 3];                % filter standard deviations
+    filtered_image = medfilt2(gray_image, sigma);
+    plotImage(filtered_image, app.axes_3);
+    
+    %% 3. adaptive histogram equalization to enhance contrast
+    contrasted_image = adapthisteq(filtered_image);
+    plotImage(contrasted_image, app.axes_4);
+
+end
+
+
+
+function plotImage(img, axes)
+    figure;
+    imshow(img);
+    %imshow(img, 'InitialMagnification', 'fit', 'Parent', axes);
+    axis image;
+    axis tight;
 end
